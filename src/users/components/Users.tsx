e9fc855a22050c5324f140eebf70c64bb846f8fb
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-05-24 20:24:10 -0300

    feat: name and email input

--- src/users/components/Users.tsx
+++ src/users/components/Users.tsx
@@ -1,7 +1,30 @@
+import { useForm } from 'react-hook-form';
+import { TextField, Stack } from '@mui/material';
+import { Schema, schema } from '../types/schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { RHFAutocomplete } from '../../components/RHFAutocomplete';
+
 export function Users() {
+
+    const { register, formState: { errors }, control } = useForm<Schema>({
+        mode: 'all',
+        resolver: zodResolver(schema),
+    });
+
     return (
         <>
-            <div>Users</div>
+            <Stack sx={{ gap: 2 }}>
+                <TextField
+                    {...register('name')}
+                    label="Name"
+                    error={!!errors.name}
+                    helperText={errors.name?.message} />
+                <TextField
+                    {...register('email')}
+                    label="Email"
+                    error={!!errors.email}
+                    helperText={errors.email?.message} />
+            </Stack>
         </>
     )
 }
